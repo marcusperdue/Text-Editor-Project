5f2688d8b617e816c14b9a36b9e82aff5f0b4bd9
Marcus Perdue <marcusperduee@gmail.com>
2023-12-05 15:06:36 -0800

    added handler for event apinstalled

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,4 +16,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('installed', 'appinstalled', event);
+});
