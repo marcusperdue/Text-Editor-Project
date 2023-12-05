8e68f3cc351a786ab7f98d009cecfab35f91bfc2
Marcus Perdue <marcusperduee@gmail.com>
2023-12-05 15:02:46 -0800

    added logic to getdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,6 +22,12 @@ export const putDb = async (content) => {
   console.log('Added content to the database:', content);
 };
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.done;
+  return content;
+};
 initdb();
